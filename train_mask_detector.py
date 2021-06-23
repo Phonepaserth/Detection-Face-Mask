fa743cad96f29fc24f143233385a86f26bddf6bb
Phonepaserth SISAYKEO <phonepaserth.ssk@gmail.com>
2021-06-23 15:30:37 +0700

    Update train_mask_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -26,7 +26,7 @@ INIT_LR = 1e-4
 EPOCHS = 20
 BS = 32
 
-DIRECTORY = r"C:\Python_OpenCV\Detection-Face-Mask\dataset"
+DIRECTORY = "C:\Python_OpenCV\Detection-Face-Mask\dataset"
 CATEGORIES = ["with_mask", "without_mask"]
 
 # grab the list of images in our dataset directory, then initialize
